e17e74ea71ca650ca0794525b89c5418b8bfc3ed
Pr1yansh1 <69392728+Pr1yansh1@users.noreply.github.com>
2023-08-01 04:50:14 +0000

    Deploying to public from @ Pr1yansh1/test-repo@566f9586684db2cab457c56845c2fe94b31bd505 🚀

--- _next/static/x4IYtlZzM6nN7xfncLfVc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/240870cf949478dd.css","static/chunks/pages/index-e1571f00aa58df68.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
