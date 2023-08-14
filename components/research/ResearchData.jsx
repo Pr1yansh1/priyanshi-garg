ae35472ae6762ba19ef346188e7173ff06d4bb6b
Pr1yansh1 <priyanshigarg068@gmail.com>
2023-08-14 14:24:39 -0400

    key changes

--- components/research/ResearchData.jsx
+++ components/research/ResearchData.jsx
@@ -9,7 +9,7 @@ const RESEARCH = [
     title: 'Undergraduate Researcher',
     date: 'Jan 2021 -- July 2021',
     bullets: [
-      <span>Worked on <InlineLink href = "https://www.cs.cmu.edu/~teachable/personnel.html"> Multi-Modal Communication for Teachable Robots </InlineLink> with Dr. Reid Simmons.</span>,
+      <span key="bullet-0">Worked on <InlineLink href = "https://www.cs.cmu.edu/~teachable/personnel.html"> Multi-Modal Communication for Teachable Robots </InlineLink> with Dr. Reid Simmons.</span>,
     ],
   },
   {
@@ -19,7 +19,7 @@ const RESEARCH = [
     title: 'Undergraduate Researcher',
     date: 'Sep 2022 -- Present',
     bullets: [
-      <span>Conducting cutting-edge research in the Machine Learning Department under the guidance of Dr. <InlineLink href="https://www.cs.cmu.edu/~nihars/">Nihar Shah</InlineLink> as an Undergraduate Researcher, focused on automating paper-reviewer assignments for academic journals.</span>,
+      <span key="bullet-1">Conducting cutting-edge research in the Machine Learning Department under the guidance of Dr. <InlineLink href="https://www.cs.cmu.edu/~nihars/">Nihar Shah</InlineLink> as an Undergraduate Researcher, focused on automating paper-reviewer assignments for academic journals.</span>,
       "Developed and tested novel online matching algorithms, informed by rigorous research, to revolutionize the review process, improve efficiency, and reduce bias.",
       "Utilized Python, APIs, and web-scraping techniques to construct a novel dataset of paper submissions and reviews, providing valuable research materials for the algorithmic assignment process.",
       "Demonstrated expertise in theoretical aspects by designing and proving the efficacy of various matching algorithms, ensuring optimal reviewer assignments based on similarity scores.",
@@ -34,10 +34,10 @@ const RESEARCH = [
     title: 'Research Developer',
     date: 'June -- August 2023',
     bullets: [
-      <span>Developed an open-source Python package for a novel trial-by-trial, spike-timing-based Bayesian decoding algorithm, revolutionizing the understanding of non-classically responsive neurons’ coding capacities in the brain.</span>,
+      <span key="bullet-2">Developed an open-source Python package for a novel trial-by-trial, spike-timing-based Bayesian decoding algorithm, revolutionizing the understanding of non-classically responsive neurons’ coding capacities in the brain.</span>,
       "Applied Object-Oriented Programming (OOP) principles and conducted rigorous unit and integration testing, resulting in a highly reliable and maintainable Python package that is now widely usable by the neuroscience community.",
       "Writing a methods paper for publication, showcasing the efficacy of the decoding package and its significant impact on advancing neuroscientific research.",
-      <span>By making the decoding algorithm accessible through an easy-to-use package, contributed to the democratization of neuroscience research, enabling scientists worldwide to investigate neuron responsiveness with greater ease and accuracy. See the package on <InlineLink href="https://github.com/InsanallyLab/DecodingPackage">GitHub</InlineLink>.</span>,
+      <span key="bullet-3">By making the decoding algorithm accessible through an easy-to-use package, contributed to the democratization of neuroscience research, enabling scientists worldwide to investigate neuron responsiveness with greater ease and accuracy. See the package on <InlineLink href="https://github.com/InsanallyLab/DecodingPackage">GitHub</InlineLink>.</span>,
     ],
   },
 ].reverse();
