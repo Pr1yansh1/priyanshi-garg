3f182f9fb5482079cdf243e590da6ed6e30226cb
Pr1yansh1 <69392728+Pr1yansh1@users.noreply.github.com>
2023-08-14 19:56:08 +0000

    Deploying to public from @ Pr1yansh1/priyanshi-garg@4a07c6a630901ab5f854f260238bd4d339bb4d9e 🚀

---
+++ _next/static/gLn5ePQ8VZvM90sz0cdzC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/872dbd072e41407f.css","static/chunks/pages/index-a2810d070e12a506.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
